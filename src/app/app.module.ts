cff7b216f2c9abe98b0a8ede19980d5e4d46fcbd
hugo1623 <hugoluis.system@gmail.com>
2022-08-29 23:56:15 -0500

    Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+// Rutas
+import {APP_ROUTING} from './app.routes'
+// Servicios
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +21,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    APP_ROUTING,
   ],
   providers: [],
   bootstrap: [AppComponent]
