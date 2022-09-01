e4c214e91a945850e93b7b143be1721c76ad74bb
hugo1623 <hugoluis.system@gmail.com>
2022-08-31 23:45:45 -0500

    RouterLink y routerLinkActive - Completando las Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 // Rutas
 import {APP_ROUTING} from './app.routes'
 // Servicios
@@ -10,6 +11,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 
 @NgModule({
@@ -23,6 +25,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule,
     APP_ROUTING,
   ],
   providers: [],
